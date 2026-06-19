366e556326e67ffc0f4802865b2f2f00e809e697
kozabrada123 <koza@o7.si>
2026-06-19 18:33:37 +0200

    fix: more auto!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/production/autonomous/CommonPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/production/autonomous/CommonPositions.java
@@ -4,7 +4,7 @@ import com.pedropathing.geometry.Pose;
 
 public class CommonPositions {
     public static final Pose BLUE_AUTO_END_POSE = new Pose(38.0, 12.0, Math.toRadians(180));
-    public static final Pose BLUE_AUTO_END_POSE_NEAR_GOAL = new Pose(18.0, 103.0, Math.toRadians(180));
+    public static final Pose BLUE_AUTO_END_POSE_NEAR_GOAL = new Pose(58.0, 114.0, Math.toRadians(180));
     public static final Pose RED_AUTO_END_POSE = new Pose(106.0, 12.0, Math.toRadians(0));
-    public static final Pose RED_AUTO_END_POSE_NEAR_GOAL = new Pose(127.0, 103.0, Math.toRadians(0));
+    public static final Pose RED_AUTO_END_POSE_NEAR_GOAL = new Pose(88.0, 114.0, Math.toRadians(0));
 }
